df10b05ab6367847d66e5c718d6dbe849fbff5f1
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-05 15:25:26 +0100

    bonus controlli input

--- js/script.js
+++ js/script.js
@@ -5,25 +5,35 @@
 // ======ins variabili dati
 
 let kmUser = parseFloat(prompt('Inserisci i km da percorrere'));
+if (isNaN(kmUser)){
+    alert ('Inserisci i km da percorrere')
+    console.log('non hai inserito un numero');
+}
 document.getElementById('kmUser').innerHTML = kmUser;
 console.log(kmUser);
 
 let ageUser = parseInt(prompt('Inserisci la tua età'));
+if (isNaN(ageUser)){
+    alert ('Inserisci la tua età')
+    console.log('non hai inserito un numero');
+}
 document.getElementById('ageUser').innerHTML = ageUser;
 console.log(ageUser);
 
 let price = kmUser * 0.21;
 
+// ============condizione di sconto per età 
+
 if (ageUser <= 18) {
     let newPrice = price - ((price * 20) / 100);
-    document.getElementById('newPrice').innerHTML = newPrice;
+    document.getElementById('newPrice').innerHTML = newPrice.toFixed(2);
     console.log(newPrice);
 } else if (ageUser >= 65) {
     let newPrice = price - ((price * 40) / 100);
-    document.getElementById('newPrice').innerHTML = newPrice;
+    document.getElementById('newPrice').innerHTML = newPrice.toFixed(2);
     console.log(newPrice);
 } else {
-    document.getElementById('price').innerHTML = price;
+    document.getElementById('price').innerHTML = price.toFixed(2);
     console.log(price)
 }
 

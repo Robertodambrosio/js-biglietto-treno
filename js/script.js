8883d5fa5e36d96452d47a65b5fa68ad9dd3ddf4
Roberto D'Ambrosio <roberto8601@gmail.com>
2021-11-05 14:58:29 +0100

    es corretto

--- js/script.js
+++ js/script.js
@@ -5,20 +5,25 @@
 // ======ins variabili dati
 
 let kmUser = parseFloat(prompt('Inserisci i km da percorrere'));
+document.getElementById('kmUser').innerHTML = kmUser;
 console.log(kmUser);
 
 let ageUser = parseInt(prompt('Inserisci la tua età'));
+document.getElementById('ageUser').innerHTML = ageUser;
 console.log(ageUser);
 
 let price = kmUser * 0.21;
 
 if (ageUser <= 18) {
-    let price18 = ((price * 20) / 100);
-    console.log(price - price18);
+    let newPrice = price - ((price * 20) / 100);
+    document.getElementById('newPrice').innerHTML = newPrice;
+    console.log(newPrice);
 } else if (ageUser >= 65) {
-    let price65 = ((price * 40) / 100);
-    console.log(price - price65);
+    let newPrice = price - ((price * 40) / 100);
+    document.getElementById('newPrice').innerHTML = newPrice;
+    console.log(newPrice);
 } else {
+    document.getElementById('price').innerHTML = price;
     console.log(price)
 }
 
